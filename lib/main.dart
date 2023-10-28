e873c3f375b681c11816262ec9555d33d9f6157c
aswindaspk <aswindaspk10@gmail.com>
2023-10-28 15:26:59 +0530

    First Project

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(home: Center(child: Text("Hello", style: TextStyle(color: Colors.blue, fontSize: 80, decoration: TextDecoration.none)))));
+}
